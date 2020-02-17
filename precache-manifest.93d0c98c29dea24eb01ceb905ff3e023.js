0be35437990df281b8d8dcebfcb4a99a34b4dc41
Andrew Appleby <andrewappleby22@gmail.com>
2020-02-16 21:13:17 -0800

    Updates

---
+++ precache-manifest.93d0c98c29dea24eb01ceb905ff3e023.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccbf387762d4c9a7deccac518ea5a17",
+    "url": "/spacex/index.html"
+  },
+  {
+    "revision": "2a0fce692ed582465c12",
+    "url": "/spacex/static/js/2.78e906f6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/spacex/static/js/2.78e906f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564b8e3d8efce844c859",
+    "url": "/spacex/static/js/main.3f2d39d2.chunk.js"
+  },
+  {
+    "revision": "dc6e43720c92ef8da7b2",
+    "url": "/spacex/static/js/runtime-main.736f0af5.js"
+  },
+  {
+    "revision": "2f525e7a4a1ffc94062a1b96467f5239",
+    "url": "/spacex/static/media/background.2f525e7a.jpg"
+  },
+  {
+    "revision": "86a30c4cc08eec9a61283b0263b430a0",
+    "url": "/spacex/static/media/checkmark.86a30c4c.svg"
+  },
+  {
+    "revision": "57b5d14140600b939dc1d6d985565fd8",
+    "url": "/spacex/static/media/link.57b5d141.svg"
+  },
+  {
+    "revision": "d301b0f29cf0e2bca35ebe703bb2788f",
+    "url": "/spacex/static/media/refresh.d301b0f2.svg"
+  }
+]);
