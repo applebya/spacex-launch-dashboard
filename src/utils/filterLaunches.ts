3eb03ec0eccedbb7266445acead62b5465f7c842
Andrew Appleby <andrewappleby22@gmail.com>
2020-02-18 10:23:51 -0800

    Reverse ordering, recent first

--- src/utils/filterLaunches.ts
+++ src/utils/filterLaunches.ts
@@ -1,25 +1,27 @@
 import { Launches, Filters } from '../reducer';
 
 const filterLaunches = (launches: Launches, filters: Filters): Launches => {
-    return launches.filter(launch => {
-        let keep = true;
+    return launches
+        .filter(launch => {
+            let keep = true;
 
-        if (filters.landSuccess) {
-            keep = Boolean(launch.rocket.first_stage.cores[0].land_success);
-        }
+            if (filters.landSuccess) {
+                keep = Boolean(launch.rocket.first_stage.cores[0].land_success);
+            }
 
-        if (keep && filters.reused) {
-            keep = Boolean(launch.rocket.first_stage.cores[0].reused);
-        }
+            if (keep && filters.reused) {
+                keep = Boolean(launch.rocket.first_stage.cores[0].reused);
+            }
 
-        if (keep && filters.withReddit) {
-            keep = Object.entries(launch.links).some(
-                ([key, val]) => key.includes('reddit') && val
-            );
-        }
+            if (keep && filters.withReddit) {
+                keep = Object.entries(launch.links).some(
+                    ([key, val]) => key.includes('reddit') && val
+                );
+            }
 
-        return keep;
-    });
+            return keep;
+        })
+        .reverse();
 };
 
 export default filterLaunches;
